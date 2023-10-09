fbbe083b1824cb3dc3d7325ba302db4f8637f061
Pawel Jelonek <pawel.jelonek.krakow@gmail.com>
2023-10-09 16:31:40 +0200

    Initial Spring Boot commit

---
+++ src/main/java/org/github/pajelonek/empik/empikgithubapi/EmpikGithubApiApplication.java
@@ -0,0 +1,13 @@
+package org.github.pajelonek.empik.empikgithubapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpikGithubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpikGithubApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/github/pajelonek/empik/empikgithubapi/EmpikGithubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.github.pajelonek.empik.empikgithubapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpikGithubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
